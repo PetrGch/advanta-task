43d0c199dc827db3ab9f6587594a44f3f36af308
Gulchuk Petr <P.Gulchuk@yandex.ru>
2017-03-31 10:24:27 +0500

    test travice

--- views/index.js
+++ views/index.js
@@ -2,6 +2,7 @@ import { Router, browserHistory } from 'react-router'
 import router from './router';
 import './index.css';
 
+
 ReactDOM.render(
     <Router routes={router} history={browserHistory} />,
     document.getElementById('root')
